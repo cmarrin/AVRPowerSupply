26c544a1826ddeb54be76e2c7624660f52ef077c
Chris Marrin <chris@marrin.com>
2014-03-23 09:20:36 -0700

    Added new features to power supply
    Support ADC to read test voltage values. You can now choose to show PS voltage and current or test voltage values on each line. Currently this is done by commenting and uncommenting lines. But eventually it will be  controlled by the Buttons and a menu. ADC takes samples from each of 4 channels and accumulates 16 samples before taking the average and storing that.
    
    Also added support Buttons. But for now these are not hooked up to anything. Documented all the port bits for the circuit. Modularized updates and display logic.

--- AVRPowerSupply.cpp
+++ AVRPowerSupply.cpp
@@ -6,6 +6,8 @@
 
 #include "m8r.h"
 
+#include "ADC.h"
+#include "Button.h"
 #include "DeviceStream.h"
 #include "EventListener.h"
 #include "FixedPoint.h"
@@ -21,13 +23,16 @@
 // This is for a power supply using a pair of MIC2941s. The regulators are manually
 // controlled, one by a 10 turn potentiometer and the other can be switched between
 // 3.3 and 5v. There is an INA129 current sensor connected to the output of each.
-// The INA129's are controlled through SPI connected to Port C4 (SDA) and C5 (SCL).
+// The INA129's are controlled through I2C connected to Port C4 (SDA) and C5 (SCL).
 // The current sensor for the variable supply is device 0 and the switchable supply
 // is device 1. The shutdown pin of the MIC2941s are connected to pin D6 (variable)
 // and D7 (switchable). The supply board also has 4 analog inputs connected to 
-// the AVR analog pins 0-3 (C0 - C3). A HD44780 compatible 2x16 text LCD is connected
-// as follows:
+// the AVR analog pins 0-3 (C0 - C3). A HD44780 compatible 2x16 text LCD is also on
+// the board. Finally there are 3 momentary on switches to ground.
 //
+// Connection:
+//
+// LCD:
 //  RS      - Port B4
 //  Enable  - Port B3
 //  D0      - Port D5
@@ -35,7 +40,13 @@
 //  D2      - Port D3
 //  D3      - Port D2
 //
-// A status LED is on port B5
+// Status LED       - port B5
+// SDA              - Port C4
+// SCL              - Port C5
+// ShutdownA (var)  - Port D6
+// ShutdownA (sw)   - Port D7
+// Analog In        - Port C0 - C3
+// Switches         - Port B0 - B2
 //
 // 
 
@@ -50,6 +61,11 @@ using namespace m8r;
 #define LCDD3 OutputBit<Port<D>, 2>
 #define ShutdownA OutputBit<Port<D>, 6>
 #define ShutdownB OutputBit<Port<D>, 7>
+#define Switch0 InputBit<Port<B>, 0>
+#define Switch1 InputBit<Port<B>, 1>
+#define Switch2 InputBit<Port<B>, 2>
+
+const uint8_t ADCAverageCount = 16;
 
 class MyApp;
 
@@ -61,21 +77,43 @@ public:
 class MyApp : public EventListener
 {    
 public:
+    friend class MyErrorReporter;
     MyApp();
     
     // EventListener override
     virtual void handleEvent(EventType type, EventParam);
     
-    StatusLED _LEDPort;
+    void updateADC();
+    void updateDisplay();
+    void showPSVoltageAndCurrent(uint8_t channel, uint8_t line);
+    void showTestVoltages(uint8_t channel0, uint8_t channel1, uint8_t line);
+    
+    void updateCurrentSensor();
+
+private:    
+    StatusLED _statusLED;
     ShutdownA _shutdownA;
     ShutdownB _shutdownB;
     DeviceStream<TextLCD<16, 2, LCD_DEFAULT, LCDRS, NullOutputBit, LCDEnable, LCDD0, LCDD1, LCDD2, LCDD3> > _lcd;
     TimerEventMgr<Timer0, TimerClockDIV64> _timerEventMgr;
-    RepeatingTimerEvent<1000> _timerEvent;
-    INA219 _current0, _current1;
-    uint16_t _busMilliVolts0, _busMilliVolts1;
-    uint16_t _shuntMicroAmps0, _shuntMicroAmps1;
-    bool _captureCurrentValues;
+    RepeatingTimerEvent<100> _timerEvent;
+    Button<Switch0> _button0;
+    Button<Switch1> _button1;
+    Button<Switch2> _button2;
+    
+    INA219 _currentSensor[2];
+    int16_t _busMilliVolts[2];
+    int16_t _shuntMilliAmps[2];
+    bool _captureSensorValues;
+    
+    bool _needsDisplay;
+
+    ADC _adc;
+    uint16_t _adcAccumulator[4];
+    uint16_t _adcVoltage[4];
+    uint8_t _adcCurrentChannel;
+    uint8_t _adcCurrentSamples;
+    bool _captureADCValue;
 
     MyErrorReporter _errorReporter;
 };
@@ -83,40 +121,127 @@ public:
 MyApp g_app;
 
 MyApp::MyApp()
-    : _current0(0x40)
-    , _current1(0x41)
-    , _captureCurrentValues(true)
+    : _captureSensorValues(true)
+    , _adc(0, ADC_PS_DIV128, ADC_REF_AVCC)
+    , _adcCurrentChannel(0)
+    , _adcCurrentSamples(0)
+    , _captureADCValue(false)
 {
+    _currentSensor[0].setAddress(0x40);
+    _currentSensor[1].setAddress(0x41);
+    _adc.setEnabled(true);
+
     sei();
     _shutdownA = false;
-    _shutdownA = false;
+    _shutdownB = false;
     System::startEventTimer(&_timerEvent);
-    _current0.setCalibration(INA219::Range16V, 40, 330);
-    _current1.setCalibration(INA219::Range16V, 40, 330);
+    _currentSensor[0].setConfiguration(INA219::Range16V);
+    _currentSensor[1].setConfiguration(INA219::Range16V);
+
+    _adc.startConversion();
 }
 
-void
-MyApp::handleEvent(EventType type, EventParam param)
+void MyApp::showPSVoltageAndCurrent(uint8_t channel, uint8_t line)
+{
+    char buf[9];
+    _lcd << TextLCDSetLine(line) << static_cast<char>(channel + 'A') << ':'
+         << FixedPoint8_8(_busMilliVolts[channel], 1000).toString(buf, 2) << FS("v ")
+         << FixedPoint8_8(_shuntMilliAmps[channel], 10).toString(buf, 1) << FS("ma");
+}
+
+void MyApp::showTestVoltages(uint8_t channel0, uint8_t channel1, uint8_t line)
+{
+    char buf[9];
+    _lcd << TextLCDSetLine(line)
+         << static_cast<char>('a' + channel0) << ':' << FixedPoint8_8(_adcVoltage[channel0], 1000).toString(buf, 2) << FS("v ")
+         << static_cast<char>('a' + channel1) << ':' << FixedPoint8_8(_adcVoltage[channel1], 1000).toString(buf, 2) << FS("v");
+}
+
+void MyApp::updateDisplay()
+{
+    if (!_needsDisplay) {
+        return;
+    }
+    _lcd << TextLCDClear();
+    _needsDisplay = false;
+    showPSVoltageAndCurrent(0, 0);
+    showPSVoltageAndCurrent(1, 1);
+//    showTestVoltages(0, 1, 0);
+//    showTestVoltages(2, 3, 1);
+}
+
+void MyApp::updateADC()
+{
+    _adcAccumulator[_adcCurrentChannel++] += _adc.lastConversion10Bit();
+    if (_adcCurrentChannel >= 4) {
+        _adcCurrentChannel = 0;
+        if (++_adcCurrentSamples >= ADCAverageCount) {
+            _adcCurrentSamples = 0;
+            for (uint8_t i = 0; i < 4; ++i) {
+                uint32_t v = (_adcAccumulator[i] + ADCAverageCount / 2) / ADCAverageCount;
+                v *= 5000;
+                v /= 1024;
+                _adcVoltage[i] = v;
+                _adcAccumulator[i] = 0;
+            }
+        }
+    }
+}
+
+void MyApp::updateCurrentSensor()
+{
+    for (uint8_t i = 0; i < 2; ++i) {
+        int16_t value = _currentSensor[i].busMilliVolts();
+        if (value != _busMilliVolts[i]) {
+            _busMilliVolts[i] = value;
+            _needsDisplay = true;
+        }
+        int32_t v = _currentSensor[i].shuntMilliVolts();
+        if (v < 0) {
+            v = 0;
+        }
+        v = (v * 100 + 165) / 330;
+        if (v != _shuntMilliAmps[i]) {
+            _shuntMilliAmps[i] = v;
+            _needsDisplay = true;
+        }
+    }
+}
+
+void MyApp::handleEvent(EventType type, EventParam param)
 {
     switch(type)
     {
         case EV_IDLE:
-        if (_captureCurrentValues) {
-            char buf[9];
-            _lcd << TextLCDClear() << TextLCDHome()
-                << FS("PS0=")
-                    << FixedPointS8_8(_current0.busMilliVolts(), 1000).toString(buf) << FS("V ")
-                    << FixedPointS8_8(_current0.shuntMicroAmps(), 1000).toString(buf) << FS("ma\n")
-                << FS("PS1=")
-                    << FixedPointS8_8(_current1.busMilliVolts(), 1000).toString(buf) << FS("V ")
-                    << FixedPointS8_8(_current1.shuntMicroAmps(), 1000).toString(buf) << FS("ma\n")
-            ;
-            _captureCurrentValues = false;
+        if (_captureSensorValues) {
+            _captureSensorValues = false;
+            updateCurrentSensor();
         }
+        if (_captureADCValue) {
+            _captureADCValue = false;
+            updateADC();
+            _adc.setChannel(_adcCurrentChannel);
+            _adc.startConversion();
+        }
+        updateDisplay();
         break;
-        case EV_EVENT_TIMER:
-            _captureCurrentValues = true;
+        case EV_ADC:
+            _captureADCValue = true;
             break;
+        case EV_EVENT_TIMER:
+            if (param == &_timerEvent) {
+                _captureSensorValues = true;
+            }
+            break;
+        case EV_BUTTON_DOWN:
+        case EV_BUTTON_UP: {
+            bool down = type == EV_BUTTON_DOWN;
+            ButtonBase* button = reinterpret_cast<ButtonBase*>(param);
+            if (button == &_button0) {
+                _statusLED = down;
+            }
+            break;
+        }
         default:
             break;
     }

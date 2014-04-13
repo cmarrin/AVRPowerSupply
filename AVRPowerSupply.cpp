f97e247fc97f9836d070090c63c30bf052d43286
Chris Marrin <chris@marrin.com>
2014-04-13 08:47:11 -0700

    Implemented menu and all functionality
    Menu now fully works. You can use the up and down arrows to change what displays on lines 1 and 2. You can also hit the select button to go into current limit setting mode. Clicking the up button switches between PS A and B. Clicking the down button takes you into adjust mode. Now the up and down arrows select among a number of current limit values. Clicking the select button asks if you want to save. Hitting the up arrow saves and any other button does not. Also hooked up the actual current limit function. Drawing current greater than the limit shuts down that supply and turns on the status LED. For now you have to reset to get out of shutdown.

--- AVRPowerSupply.cpp
+++ AVRPowerSupply.cpp
@@ -62,9 +62,9 @@ using namespace m8r;
 #define LCDD3 OutputBit<Port<D>, 2>
 #define ShutdownA OutputBit<Port<D>, 6>
 #define ShutdownB OutputBit<Port<D>, 7>
-#define Switch0 InputBit<Port<B>, 0>
-#define Switch1 InputBit<Port<B>, 1>
-#define Switch2 InputBit<Port<B>, 2>
+#define Switch0 DynamicInputBit<B, 0>
+#define Switch1 DynamicInputBit<B, 1>
+#define Switch2 DynamicInputBit<B, 2>
 
 const uint8_t ADCAverageCount = 16;
 
@@ -72,10 +72,17 @@ class MyApp;
 
 class MyErrorReporter : public ErrorReporter {
 public:
-    virtual void reportError(char c, uint16_t, ErrorConditionType);
+    virtual void reportError(char c, uint32_t, ErrorConditionType);
 };
 
-class MyApp : public EventListener
+const char bannerString[] PROGMEM = "AVR Power Supply\n      v0.1";
+const char curLimit[] PROGMEM = "Cur Limit";
+const char accept[] PROGMEM = "Save? (UP=YES)";
+const char accepted[] PROGMEM = "Cur Limit Set";
+const uint8_t curLimitValues[] PROGMEM = { 1, 5, 10, 20, 40, 60, 80, 100 };
+const uint8_t numCurLimitValues = sizeof(curLimitValues);
+
+class MyApp : public EventListener, public Menu<MyApp>
 {    
 public:
     friend class MyErrorReporter;
@@ -90,22 +97,80 @@ public:
     void showPSCurrents(uint8_t line);
     void showTestVoltages(uint8_t channel0, uint8_t channel1, uint8_t line);
     
+    enum class CurrentLimitArrow { None, Supply, Current };
+    void showCurrentLimit(uint8_t supply, CurrentLimitArrow);
+    
     void updateCurrentSensor();
 
+    // Menu
+    virtual void show(const _FlashString& s)
+    {
+        _displayEnabled = false;
+        _lcd << TextLCDClear() << s;
+    }
+
+    static void display(MyApp* app)
+    {
+        app->_displayEnabled = true;
+    }
+    static void nextLine0(MyApp* app) { app->advanceLineDisplay(0); }
+    static void nextLine1(MyApp* app) { app->advanceLineDisplay(1); }
+    static void curLimit0(MyApp* app) { app->showCurrentLimit(0, CurrentLimitArrow::Supply); app->_currentLimitAdjustSupply = 0; }
+    static void curLimit1(MyApp* app) { app->showCurrentLimit(1, CurrentLimitArrow::Supply); app->_currentLimitAdjustSupply = 1; }
+    static void adjustCurLimit(MyApp* app) { app->showCurrentLimit(app->_currentLimitAdjustSupply, CurrentLimitArrow::Current); }
+    static void showCurLimit(MyApp* app) { app->showCurrentLimit(app->_currentLimitAdjustSupply, CurrentLimitArrow::None); }
+    static void incCurLimit(MyApp* app)
+    {
+        if (++app->_currentLimitAdjustIndex[app->_currentLimitAdjustSupply] >= numCurLimitValues) {
+            app->_currentLimitAdjustIndex[app->_currentLimitAdjustSupply] = 0;
+        }
+    }
+    static void decCurLimit(MyApp* app)
+    {
+        if (app->_currentLimitAdjustIndex[app->_currentLimitAdjustSupply]-- == 0) {
+            app->_currentLimitAdjustIndex[app->_currentLimitAdjustSupply] = numCurLimitValues - 1;
+        }
+    }
+    static void acceptCurLimit(MyApp* app)
+    {
+        app->_currentLimitIndex[0] = app->_currentLimitAdjustIndex[0];
+        app->_currentLimitIndex[1] = app->_currentLimitAdjustIndex[1];
+    }
+    static void rejectCurLimit(MyApp* app)
+    {
+        app->_currentLimitAdjustIndex[0] = app->_currentLimitIndex[0];
+        app->_currentLimitAdjustIndex[1] = app->_currentLimitIndex[1];
+    }
+
 private:    
+    void advanceLineDisplay(uint8_t line)
+    {
+        _lineDisplayMode[line] = (LineDisplayMode)((uint8_t)_lineDisplayMode[line] + 1);
+        if (_lineDisplayMode[line] == LineDisplayMode::Last) {
+            _lineDisplayMode[line] = LineDisplayMode::PS1VA;
+        }
+        _needsDisplay = true;
+    }
+    
+    uint16_t curLimitMa(uint8_t supply) const { return (uint16_t) pgm_read_byte(&curLimitValues[_currentLimitAdjustIndex[supply]]) * 10; }
+
+
+    MyErrorReporter _errorReporter;
+
     StatusLED _statusLED;
     ShutdownA _shutdownA;
     ShutdownB _shutdownB;
     DeviceStream<TextLCD<16, 2, LCD_DEFAULT, LCDRS, NullOutputBit, LCDEnable, LCDD0, LCDD1, LCDD2, LCDD3> > _lcd;
     TimerEventMgr<Timer0, TimerClockDIV64> _timerEventMgr;
-    RepeatingTimerEvent<100> _timerEvent;
+    RepeatingTimerEvent _timerEvent;
     
     INA219 _currentSensor[2];
     int16_t _busMilliVolts[2];
     int16_t _shuntMilliAmps[2];
     bool _captureSensorValues;
     
-    bool _needsDisplay;
+    bool _needsDisplay = true;
+    bool _displayEnabled = false;
 
     ADC _adc;
     uint16_t _adcAccumulator[4];
@@ -114,35 +179,55 @@ private:
     uint8_t _adcCurrentSamples;
     bool _captureADCValue;
     
-    enum LineDisplayMode { LDMPS1VA, LDMPS2VA, LDMPS12A, LDMV1V2, LDMV3V4, LDMLast };
+    uint8_t _currentLimitIndex[2];
+    uint8_t _currentLimitAdjustIndex[2];
+    uint8_t _currentLimitAdjustSupply = 0;
+    
+    enum class LineDisplayMode { PS1VA, PS2VA, PS12A, V1V2, V3V4, Last };
 
     LineDisplayMode _lineDisplayMode[2];
+
+    ButtonSet<Switch0, Switch1, Switch2> _buttons;
+};
+
+typedef Menu<MyApp> MyMenu;
+const MenuOpType g_menuOps[] = {
+    MyMenu::Show(bannerString), MyMenu::Pause(2000),
     
-    Button<Switch0> _button0;
-    Button<Switch1> _button1;
-    Button<Switch2> _button2;
-    ButtonSet<3> _buttons;
-    Menu _menu;
-    
-    MyErrorReporter _errorReporter;
+    MyMenu::State( 0), MyMenu::XEQ(MyApp::display), MyMenu::Buttons(), 1, 2, 3,         // Normal display
+    MyMenu::State( 1), MyMenu::XEQ(MyApp::nextLine0), MyMenu::Goto(0),                  // Show next display for line 0
+    MyMenu::State( 2), MyMenu::XEQ(MyApp::nextLine1), MyMenu::Goto(0),                  // Show next display for line 1
+    MyMenu::State( 3), MyMenu::Show(curLimit), MyMenu::Goto(4),                         // Show cur limit
+    MyMenu::State( 4), MyMenu::XEQ(MyApp::curLimit0), MyMenu::Buttons(), 5, 6, 0,       // Set cur limit adjust to supply A
+    MyMenu::State( 5), MyMenu::XEQ(MyApp::curLimit1), MyMenu::Buttons(), 4, 6, 0,       // Set cur limit adjust to supply B
+    MyMenu::State( 6), MyMenu::XEQ(MyApp::adjustCurLimit), MyMenu::Buttons(), 7, 8, 9,  // Start cur limit adjust
+    MyMenu::State( 7), MyMenu::XEQ(MyApp::incCurLimit), MyMenu::Goto(6),                // inc cur limit
+    MyMenu::State( 8), MyMenu::XEQ(MyApp::decCurLimit), MyMenu::Goto(6),                // dec cur limit
+    MyMenu::State( 9), MyMenu::Show(accept), MyMenu::XEQ(MyApp::showCurLimit),          // Ask to accept new cur limit settings
+                       MyMenu::Buttons(), 10, 11, 11,
+    MyMenu::State(10), MyMenu::Show(accepted), MyMenu::XEQ(MyApp::acceptCurLimit),      // Accept new cur limit settings
+                       MyMenu::Pause(2000), MyMenu::Goto(0),
+    MyMenu::State(11), MyMenu::XEQ(MyApp::rejectCurLimit), MyMenu::Goto(0),             // Reject new cur limit settings
+    MyMenu::End()
 };
 
 MyApp g_app;
 
 MyApp::MyApp()
-    : _captureSensorValues(true)
+    : Menu(&_buttons, g_menuOps, this)
+    , _timerEvent(100)
+    , _captureSensorValues(true)
     , _adc(0, ADC_PS_DIV128, ADC_REF_AVCC)
     , _adcCurrentChannel(0)
     , _adcCurrentSamples(0)
     , _captureADCValue(false)
-    , _buttons(&_button0, &_button1, &_button2)
-    , _menu(&_buttons)
+    , _currentLimitIndex{ numCurLimitValues - 1, numCurLimitValues - 1 }
+    , _currentLimitAdjustIndex{ numCurLimitValues - 1, numCurLimitValues - 1 }
+    , _lineDisplayMode{ LineDisplayMode::PS1VA, LineDisplayMode::PS2VA }
 {
     _currentSensor[0].setAddress(0x40);
     _currentSensor[1].setAddress(0x41);
     _adc.setEnabled(true);
-    _lineDisplayMode[0] = LDMPS12A;
-    _lineDisplayMode[1] = LDMV1V2;
 
     sei();
     _shutdownA = false;
@@ -156,31 +241,37 @@ MyApp::MyApp()
 
 void MyApp::showPSVoltageAndCurrent(uint8_t channel, uint8_t line)
 {
-    char buf[9];
-    _lcd << TextLCDSetLine(line) << static_cast<char>(channel + 'A') << ':'
-         << FixedPoint8_8(_busMilliVolts[channel], 1000).toString(buf, 2) << FS("v ")
-         << FixedPoint8_8(_shuntMilliAmps[channel], 10).toString(buf, 1) << FS("ma");
+    _lcd << TextLCDSetLine(line) << static_cast<char>(channel + 'A') << ':';
+    _lcd << FixedPoint8_8(_busMilliVolts[channel], 1000).toString(2) << FS("v ");
+    _lcd << FixedPoint8_8(_shuntMilliAmps[channel], 10).toString(1) << FS("ma");
 }
 
 void MyApp::showPSCurrents(uint8_t line)
 {
-    char buf[9];
-    _lcd << TextLCDSetLine(line)
-         << FS("A:") << FixedPoint8_8(_shuntMilliAmps[0], 10).toString(buf, 1) << FS("ma")
-         << FS(" B:") << FixedPoint8_8(_shuntMilliAmps[1], 10).toString(buf, 1) << FS("ma");
+    _lcd << TextLCDSetLine(line);
+    _lcd << FS("A:") << FixedPoint8_8(_shuntMilliAmps[0], 10).toString(1) << FS("ma");
+    _lcd << FS(" B:") << FixedPoint8_8(_shuntMilliAmps[1], 10).toString(1) << FS("ma");
 }
 
 void MyApp::showTestVoltages(uint8_t channel0, uint8_t channel1, uint8_t line)
 {
-    char buf[9];
-    _lcd << TextLCDSetLine(line)
-         << static_cast<char>('a' + channel0) << ':' << FixedPoint8_8(_adcVoltage[channel0], 1000).toString(buf, 2) << FS("v ")
-         << static_cast<char>('a' + channel1) << ':' << FixedPoint8_8(_adcVoltage[channel1], 1000).toString(buf, 2) << FS("v");
+    _lcd << TextLCDSetLine(line);
+    _lcd << static_cast<char>('a' + channel0) << ':' << FixedPoint8_8(_adcVoltage[channel0], 1000).toString(2) << FS("v ");
+    _lcd << static_cast<char>('a' + channel1) << ':' << FixedPoint8_8(_adcVoltage[channel1], 1000).toString(2) << FS("v");
+}
+
+void MyApp::showCurrentLimit(uint8_t supply, CurrentLimitArrow arrow)
+{
+    _displayEnabled = false;
+    _lcd << TextLCDClearLine(1)
+         << ((arrow == CurrentLimitArrow::Supply) ? '\x7e' : ' ')
+         << static_cast<char>('A' + supply) << ':' << curLimitMa(supply) << FS("ma")
+         << ((arrow == CurrentLimitArrow::Current) ? '\x7f' : ' ');
 }
 
 void MyApp::updateDisplay()
 {
-    if (!_needsDisplay) {
+    if (!_displayEnabled || !_needsDisplay) {
         return;
     }
     _lcd << TextLCDClear();
@@ -188,11 +279,11 @@ void MyApp::updateDisplay()
     
     for (uint8_t i = 0; i < 2; ++i) {
         switch(_lineDisplayMode[i]) {
-            case LDMPS1VA: showPSVoltageAndCurrent(0, i); break;
-            case LDMPS2VA: showPSVoltageAndCurrent(1, i); break;
-            case LDMPS12A: showPSCurrents(i); break;
-            case LDMV1V2: showTestVoltages(0, 1, i); break;
-            case LDMV3V4: showTestVoltages(2, 3, i); break;
+            case LineDisplayMode::PS1VA: showPSVoltageAndCurrent(0, i); break;
+            case LineDisplayMode::PS2VA: showPSVoltageAndCurrent(1, i); break;
+            case LineDisplayMode::PS12A: showPSCurrents(i); break;
+            case LineDisplayMode::V1V2: showTestVoltages(0, 1, i); break;
+            case LineDisplayMode::V3V4: showTestVoltages(2, 3, i); break;
             default: break;
         }
     }
@@ -231,6 +322,14 @@ void MyApp::updateCurrentSensor()
         v = (v * 100 + 165) / 330;
         if (v != _shuntMilliAmps[i]) {
             _shuntMilliAmps[i] = v;
+            if (v > curLimitMa(i)) {
+                if (i == 0) {
+                    _shutdownA = true;
+                } else {
+                    _shutdownB = true;
+                }
+                _statusLED = true;
+            }
             _needsDisplay = true;
         }
     }
@@ -238,7 +337,7 @@ void MyApp::updateCurrentSensor()
 
 void MyApp::handleEvent(EventType type, EventParam param)
 {
-    _menu.handleEvent(type, param);
+    Menu::handleEvent(type, param);
     
     switch(type) {
         case EV_IDLE:
@@ -253,7 +352,6 @@ void MyApp::handleEvent(EventType type, EventParam param)
             _adc.startConversion();
         }
         updateDisplay();
-        _statusLED = _menu.buttonState(0);
         break;
         case EV_ADC:
             _captureADCValue = true;
@@ -263,7 +361,8 @@ void MyApp::handleEvent(EventType type, EventParam param)
                 _captureSensorValues = true;
             }
             break;
-//        case EV_BUTTON_DOWN: {
+        case EV_BUTTON_DOWN:
+            break;
 //            ButtonBase* button = reinterpret_cast<ButtonBase*>(param);
 //            if (button == &_button0) {
 //                _lineDisplayMode[0] = (LineDisplayMode)((uint8_t)_lineDisplayMode[0] + 1);
@@ -286,15 +385,40 @@ void MyApp::handleEvent(EventType type, EventParam param)
     }
 }
 
-void
-MyErrorReporter::reportError(char c, uint16_t code, ErrorConditionType type)
+static char* toHex(char* buf, uint32_t u)
 {
+    buf += 10;
+    *--buf = '\0';
+    if (u == 0) {
+        *--buf = '0';
+        *--buf = '0';
+    } else {
+        while (u) {
+            uint8_t c = u & 0xf;
+            *--buf = (c > 9) ? (c + 'a' - 10) : (c + '0');
+            u >>= 4;
+            c = u & 0xf;
+            *--buf = (c > 9) ? (c + 'a' - 10) : (c + '0');
+            u >>= 4;
+        }
+    }
+    *--buf = 'x';
+    *--buf = '0';
+    return buf;
+}
+
+void
+MyErrorReporter::reportError(char c, uint32_t code, ErrorConditionType type)
+{
+    g_app._lcd << TextLCDClear();
     switch(type) {
         case ErrorConditionNote: g_app._lcd << "Note:"; break;
         case ErrorConditionWarning: g_app._lcd << "Warn:"; break;
         case ErrorConditionFatal: g_app._lcd << "Fatl:"; break;
     }
-    g_app._lcd << code;
+    char buf[12];
+    char* p = toHex(buf, code);
+    g_app._lcd << p;
     if (type == ErrorConditionFatal)
         while (1) ;
     
